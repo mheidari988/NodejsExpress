705644e8c29cf68716dd78b05686f6c99ba8ead9
Reza Heidari <reza.heidari@amplicade.com>
2023-01-16 22:38:03 +0300

    add nodemon config

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = express();
 
 server.use(express.static(path.join(__dirname, '/public')));
