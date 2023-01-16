d4d6b4301d901da68806c382d97be1e7c52b09e0
Reza Heidari <reza.heidari@amplicade.com>
2023-01-16 22:20:40 +0300

    server html and use morgan

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
 const port = 3000;
 const server = express();
+
+server.use(express.static(path.join(__dirname, '/public')));
+server.use(morgan('tiny'));
 
 server.get('/', (req, res) => {
     res.send('Hello from express server');
 });
 
 server.listen(port, () => {
-    console.log(`Server is listening on port ${port}...`);
+    console.log(`Server is listening on port ${port}`);
 })
